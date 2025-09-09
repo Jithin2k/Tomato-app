e1420f443144b28921968ea6635e1d87ac1e88a5
Jithin2k <jithinjmadhav@gmail.com>
2025-09-09 15:20:54 +0530

    Create Menu item list and Item Card

---
+++ src/Components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import { StoreContext } from "../../Context/StoreContext";
+import FoodItem from "../FoodItem.jsx/FoodItem";
+import "./FoodDisplay.css"
+
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext);
+  return (
+    <div className="food-display" id="food-display">
+      <h2>Top dishes for you</h2>
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          return (
+            <FoodItem
+            key={index}
+              id={item.id}
+              name={item.name}
+              description={item.description}
+              image={item.image}
+              price={item.price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default FoodDisplay;
