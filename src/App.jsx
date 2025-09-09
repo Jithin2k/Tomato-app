fd9863e90ead306c956900766eab2d67cadc47fa
Jithin2k <jithinjmadhav@gmail.com>
2025-09-09 13:17:00 +0530

    Create Header Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import "./index.css"
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import "./index.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Cart from "./Pages/Cart/Cart";
+import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
+
 
 const App = () => {
   return (
-    <div className='app'>
-    <Navbar/>
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
