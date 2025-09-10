48b66650e0bff13c3b3e3684cdd784d0fc475f9e
Jithin2k <jithinjmadhav@gmail.com>
2025-09-10 10:44:40 +0530

    Create footer and app download component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Cart from "./Pages/Cart/Cart";
 import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
+import Footer from "./Components/Footer/Footer";
 
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/cart" element={<Cart />} />
         <Route path="/order" element={<PlaceOrder />} />
       </Routes>
+      <Footer/>
     </div>
   );
 };
